ecd6084de9423b63267ae81b830efe0b49dd32b4
Benjamin Deconinck <bdbenboss@hotmail.com>
2022-09-22 16:44:51 +0200

    validate, relation finish

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,7 @@
 class Booking < ApplicationRecord
+  belongs_to :user
+  belongs_to :car
+
+  validates :start_at, presence: true
+  validates :ends_at, presence: true
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,9 @@
 class Car < ApplicationRecord
+  has_many :bookings
+  has_many :users
+
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :address, prescence: true
+  validates_numericality_of :price_per_day, :greater_than => 0
 end
