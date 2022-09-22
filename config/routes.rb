4ff8208f2a1a12fc65e38fe24296d300dbf6d6c5
Benjamin Deconinck <bdbenboss@hotmail.com>
2022-09-22 15:57:30 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'pages#home'
+  get 'car', to: 'pages#car'
+  get 'info', to: 'pages#info'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
